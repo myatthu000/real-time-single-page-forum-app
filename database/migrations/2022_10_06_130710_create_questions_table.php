1a90cf6ba035dc9b220f19d9ef594fffbff2e1ce
myatthu000 <september.2019thu@gmail.com>
2022-10-06 20:33:50 +0630

    Controller, Models, Factory, Migrations

---
+++ database/migrations/2022_10_06_130710_create_questions_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateQuestionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('questions', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('slug');
+
+            $table->longText('body');
+
+            $table->unsignedInteger('category_id');
+            $table->unsignedInteger('user_id');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('questions');
+    }
+}
