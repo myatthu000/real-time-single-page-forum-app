9c763d644ef9a6d172279540513164136bbbf089
myatthu000 <september.2019thu@gmail.com>
2022-10-07 09:11:12 +0630

    Data seed, Api route setup

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
+use App\Models\Like;
+use App\Models\Question;
+use App\Models\Reply;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +18,16 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        User::factory(10)->create();
+        Category::factory(5)->create();
+        Question::factory(10)->create();
+
+        // Whenever we create reply
+        // Each reply has 1 like or likes
+        // ** That will create only one like/likes on each reply **
+
+        Reply::factory(50)->create()->each(function ($reply){
+            return $reply->like()->save(Like::factory()->make());
+        });
     }
 }
